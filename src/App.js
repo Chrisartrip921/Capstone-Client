4a7576bd8058f4623f9d18145e6877e2f242c31e
chris artrip <christopherartrip@gmail.com>
2021-01-25 11:58:31 -0500

    night mode implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
@@ -14,12 +15,31 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import SinglePost from './pages/SinglePost';
 
+const GlobalStyle = createGlobalStyle `
+body{
+  background-color: ${props => 
+    props.theme.mode === 'dark' ? '#111' : '#EEE'};
+  color: ${props => 
+    props.theme.mode === 'dark' ? '#EEE' : '#111'};
+  }
+}
+`;
 function App() {
+  const [theme, setTheme] = useState({mode: 'light'})
   return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
     <AuthProvider>
       <Router>
         <Container>
           <MenuBar />
+          <button onClick = {e => 
+            setTheme(
+              theme.mode === 'dark'
+              ? { mode: 'light'}
+              : { mode: 'dark'}
+            )
+          } >Press Me!</button>
           <Route exact path="/" component={Home} />
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
@@ -27,6 +47,7 @@ function App() {
         </Container>
       </Router>
     </AuthProvider>
+    </ThemeProvider>
   );
 }
 
